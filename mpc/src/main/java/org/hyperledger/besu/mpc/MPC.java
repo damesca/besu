6ce94b748a4df9c9404fcb826515f0c2894a10d4
Daniel Morales <damesca@lcc.uma.es>
2022-11-24 10:42:45 +0100

    Added new module /mpc to the project. This module will be a wrapper for external mpc libraries.

---
+++ mpc/src/main/java/org/hyperledger/besu/mpc/MPC.java
@@ -0,0 +1,11 @@
+package org.hyperledger.besu.mpc;
+
+import edu.alibaba.mpc4j.common.tool.CommonConstants;
+
+public class MPC {
+    
+    public String hello() {
+        return CommonConstants.DEFAULT_CHARSET.toString();
+    }
+
+}
